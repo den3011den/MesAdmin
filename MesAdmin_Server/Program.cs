cbbc8db13cee1fe8c2076d4b5a8e0813b467edea
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-05-20 00:57:42 +0700

    Удаление лишнего. Подготовка меню для ResourceManagement

--- MesAdmin_Server/Program.cs
+++ MesAdmin_Server/Program.cs
@@ -1,4 +1,3 @@
-using MesAdmin_Server.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
@@ -17,6 +15,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
