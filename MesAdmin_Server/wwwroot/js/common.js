8cdfd4d991781ca71f3c5fc256ebd4ef032bfe45
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-05-26 03:33:31 +0700

    Подключение и тест Toastr.

---
+++ MesAdmin_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Операция выполнена успешно", { timeOut: 5000});
+    }
+    if (type === "error") {
+        toastr.error(message, "Ошибка выполнения операции", { timeOut: 5000 });
+    }
+}
