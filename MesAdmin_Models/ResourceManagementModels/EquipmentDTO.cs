640bdd36d43745a85cc493ac76a31f4ea611e9f7
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-05-27 23:40:59 +0700

    Подготовка объектов данных, отражающих записи из таблиц баз данных.

---
+++ MesAdmin_Models/ResourceManagementModels/EquipmentDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesAdmin_Models.ResourceManagementModels
+{
+    public class EquipmentDTO
+    {
+        [Display(Name = "Код оборудования")]        
+        public Guid EquipmentId { get; set; }
+
+        [Display(Name = "Наименование оборудования s95Id")]
+        [StringLength(50, ErrorMessage = "Наименование не может быть больше 50 символов")]
+        public string? S95Id { get; set; }
+        [Display(Name = "Описание оборудования")]
+        [StringLength(255, ErrorMessage = "Описание оборудования не может быть больше 255 символов")]
+        public string? Description { get; set; }
+        [Display(Name = "Тип")]
+        [StringLength(255, ErrorMessage = "Тип не может быть больше 255 символов")]
+        public string? Type { get; set; }
+        [Display(Name = "Версия")]
+        public long? Version { get; set; }
+        [Display(Name = "Родитель")]
+        public EquipmentDTO ParentEquipment { get; set; }
+    }
+}
