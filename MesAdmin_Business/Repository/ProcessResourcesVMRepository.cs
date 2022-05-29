6c230b7b2bca55c98fb628000d5d4376635a566c
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-05-29 19:09:01 +0700

    Реализация AddEquipment и DeleteEquipment в ProcessResourcesVMRepository

--- MesAdmin_Business/Repository/ProcessResourcesVMRepository.cs
+++ MesAdmin_Business/Repository/ProcessResourcesVMRepository.cs
@@ -5,6 +5,7 @@ using MesAdmin_DataAccess.Data.RPMData;
 using MesAdmin_DataAccess.Data.RPMData.ViewModel;
 using MesAdmin_DataAccess.Data.SOADB;
 using MesAdmin_Models.ResourceManagementModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +31,30 @@ namespace MesAdmin_Business.Repository
 
         public async Task<ProcessResourcesVMDTO> AddEquipment(string ParentId, string InsideId, Guid EquipGuid)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var objProcessResourceFromDb = _dbRPMData.ProcessResourcesDbSet.FirstOrDefault(u => (u.ParentId == ParentId && u.InsideId == InsideId));
+                if (objProcessResourceFromDb != null)
+                {
+                    if (!objProcessResourceFromDb.EquipmentsData.ToLower().Contains(EquipGuid.ToString().ToLower()))
+                    {
+                        List<Guid> equipmentsDataList = SD.GetEquipmentListFromEquipmentsDataString(objProcessResourceFromDb.EquipmentsData);
+                        equipmentsDataList.Add(EquipGuid);
+                        string equipmentsDataString = SD.GetEquipmentsDataStringFromEquipmentList(equipmentsDataList);
+                        objProcessResourceFromDb.EquipmentsData = equipmentsDataString;
+                        await _dbRPMData.SaveChangesAsync();
+                    }
+
+                    ProcessResourcesVMDTO processResourcesVMDTO = await Get(ParentId, InsideId);
+                    return processResourcesVMDTO;
+                }
+                else
+                    return new ProcessResourcesVMDTO();
+            }
+            catch (Exception ex)                                        
+            {
+                return new ProcessResourcesVMDTO();
+            }            
         }
 
         public async Task<ProcessResourcesVMDTO> Create(ProcessResourcesVMDTO objDTO)
@@ -67,15 +91,35 @@ namespace MesAdmin_Business.Repository
 
         public async Task<ProcessResourcesVMDTO> DeleteEquipment(string ParentId, string InsideId, Guid EquipGuid)
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                var objProcessResourceFromDb = _dbRPMData.ProcessResourcesDbSet.FirstOrDefault(u => (u.ParentId == ParentId && u.InsideId == InsideId));
+                if (objProcessResourceFromDb != null)
+                {
+                    if (objProcessResourceFromDb.EquipmentsData.ToLower().Contains(EquipGuid.ToString().ToLower()))
+                    {
+                        List<Guid> equipmentsDataList = SD.GetEquipmentListFromEquipmentsDataString(objProcessResourceFromDb.EquipmentsData);
+                        equipmentsDataList.Remove(EquipGuid);
+                        string equipmentsDataString = SD.GetEquipmentsDataStringFromEquipmentList(equipmentsDataList);
+                        objProcessResourceFromDb.EquipmentsData = equipmentsDataString;
+                        await _dbRPMData.SaveChangesAsync();
+                    }                
+                }
+                ProcessResourcesVMDTO processResourcesVMDTO = await Get(ParentId, InsideId);
+                return processResourcesVMDTO;
+            }
+            catch (Exception ex)
+            {
+                return new ProcessResourcesVMDTO();
+            }
+        }    
 
-        public async Task<ProcessResourcesVMDTO> Get(string ParentId, string InsideId)
+    public async Task<ProcessResourcesVMDTO> Get(string ParentId, string InsideId)
         {
 
             ProcessResourcesVM processResourcesVM = new();
 
-            ProcessResources processResourcesFromDb = _dbRPMData.ProcessResourcesDbSet.FirstOrDefault(u => (u.ParentId == ParentId) && (u.InsideId == InsideId));
+            var processResourcesFromDb = await _dbRPMData.ProcessResourcesDbSet.FirstOrDefaultAsync(u => (u.ParentId == ParentId) && (u.InsideId == InsideId));
             if (processResourcesFromDb == null)
             {
                 return new ProcessResourcesVMDTO();
@@ -93,7 +137,7 @@ namespace MesAdmin_Business.Repository
 
                 foreach(var guid in EquipmentsData)
                 {
-                    Equipment equipmentFromDb = _dbSAODB.EquipmentDbSet.FirstOrDefault(u => u.EquipmentId == guid);
+                    var equipmentFromDb = await _dbSAODB.EquipmentDbSet.FirstOrDefaultAsync(u => u.EquipmentId == guid);
                     if (equipmentFromDb != null)
                     {
                         equipmentListFromDb.Add(equipmentFromDb);
@@ -130,7 +174,8 @@ namespace MesAdmin_Business.Repository
 
                 foreach (var guid in EquipmentsDataList)
                 {
-                    Equipment equipmentFromDb = _dbSAODB.EquipmentDbSet.FirstOrDefault(u => u.EquipmentId == guid);
+                    var equipmentFromDb = await _dbSAODB.EquipmentDbSet.FirstOrDefaultAsync(u => u.EquipmentId == guid);
+                    
                     if (equipmentFromDb != null)
                     {
                         equipmentListFromDb.Add(equipmentFromDb);
@@ -148,20 +193,33 @@ namespace MesAdmin_Business.Repository
         {
             if (objDTO != null)
             {
-                var ProcessResourcesFromDb = _dbRPMData.ProcessResourcesDbSet.FirstOrDefault(u => u.ParentId == objDTO.ParentId && u.InsideId == objDTO.InsideId);
-                ProcessResourcesFromDb.ParentId = objDTO.ParentId;
-                ProcessResourcesFromDb.InsideId = objDTO.InsideId;
-                ProcessResourcesFromDb.Description = objDTO.Description;
-                ProcessResourcesFromDb.IsStorage = objDTO.IsStorage;
-                ProcessResourcesFromDb.ResourceName = objDTO.ResourceName;
-                ProcessResourcesFromDb.Department = objDTO.Department;
-                ProcessResourcesFromDb.IsProduction = objDTO.IsProduction;
-                //ProcessResourcesFromDb.EquipmentsData = objDTO.EquipmentsData;
+                try
+                {
+                    var ProcessResourcesFromDb = await _dbRPMData.ProcessResourcesDbSet.FirstOrDefaultAsync(u => u.ParentId == objDTO.ParentId && u.InsideId == objDTO.InsideId);
+                    if (ProcessResourcesFromDb != null)
+                    {
+                        ProcessResourcesFromDb.ParentId = objDTO.ParentId;
+                        ProcessResourcesFromDb.InsideId = objDTO.InsideId;
+                        ProcessResourcesFromDb.Description = objDTO.Description;
+                        ProcessResourcesFromDb.IsStorage = objDTO.IsStorage;
+                        ProcessResourcesFromDb.ResourceName = objDTO.ResourceName;
+                        ProcessResourcesFromDb.Department = objDTO.Department;
+                        ProcessResourcesFromDb.IsProduction = objDTO.IsProduction;
+                        //ProcessResourcesFromDb.EquipmentsData = objDTO.EquipmentsData;
 
-                await _dbRPMData.SaveChangesAsync();
-                return _mapper.Map<ProcessResources, ProcessResourcesDTO>(ProcessResourcesFromDb);
+                        await _dbRPMData.SaveChangesAsync();
+                        return _mapper.Map<ProcessResources, ProcessResourcesDTO>(ProcessResourcesFromDb);
+                    }
+                    else
+                        return new ProcessResourcesDTO();
+                }
+                catch (Exception ex)
+                {
+                    return new ProcessResourcesDTO();
+                }
             }
-            return new ProcessResourcesDTO();
+            else
+                return new ProcessResourcesDTO();
         }
     }
 }
